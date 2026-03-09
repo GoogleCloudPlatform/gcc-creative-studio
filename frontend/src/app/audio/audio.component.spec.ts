9f25a7313212903020d14ebe62d048b8e1bac059
Pradhyumna Pawar <pradhyumnap@google.com>
2026-03-09 11:48:14 +0000

    Update frontend/src/app/audio/audio.component.spec.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/app/audio/audio.component.spec.ts
+++ frontend/src/app/audio/audio.component.spec.ts
@@ -175,9 +175,7 @@ describe('AudioComponent', () => {
         { provide: NotificationService, useValue: notificationServiceSpy },
       ],
     })
-    .compileComponents();
-    })
-    .compileComponents();
+      .compileComponents();
     setAppInjector(TestBed.inject(Injector));
     fixture = TestBed.createComponent(AudioComponent);
     component = fixture.componentInstance;
