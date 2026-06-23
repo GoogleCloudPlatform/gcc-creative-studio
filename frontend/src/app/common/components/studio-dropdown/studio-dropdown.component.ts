418f23e3c49441c534ba34ee49c2e105a1db7cd8
Edgar Trujillo <tedgar@google.com>
2026-06-23 20:48:44 +0000

    merge with test

--- frontend/src/app/common/components/studio-dropdown/studio-dropdown.component.ts
+++ frontend/src/app/common/components/studio-dropdown/studio-dropdown.component.ts
@@ -23,7 +23,7 @@ import {
   ElementRef,
   HostBinding,
 } from '@angular/core';
-import {trigger, state, style, transition, animate} from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 export interface DropdownOption {
   value: any;
@@ -31,7 +31,7 @@ export interface DropdownOption {
   color?: string;
   icon?: string;
   isSvgIcon?: boolean;
-  deletable?: boolean;
+  tooltip?: string;
 }
 
 @Component({
@@ -41,17 +41,17 @@ export interface DropdownOption {
   animations: [
     trigger('dropdownAnimation', [
       transition(':enter', [
-        style({height: '0px', opacity: 0, overflow: 'hidden'}),
+        style({ height: '0px', opacity: 0, overflow: 'hidden' }),
         animate(
           '250ms cubic-bezier(0.4, 0, 0.2, 1)',
-          style({height: '*', opacity: 1}),
+          style({ height: '*', opacity: 1 }),
         ),
       ]),
       transition(':leave', [
-        style({height: '*', opacity: 1, overflow: 'hidden'}),
+        style({ height: '*', opacity: 1, overflow: 'hidden' }),
         animate(
           '250ms cubic-bezier(0.4, 0, 0.2, 1)',
-          style({height: '0px', opacity: 0}),
+          style({ height: '0px', opacity: 0 }),
         ),
       ]),
     ]),
@@ -97,7 +97,7 @@ export class StudioDropdownComponent {
     return `size-${this.size}`;
   }
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef) { }
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
