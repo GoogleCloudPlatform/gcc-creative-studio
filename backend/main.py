c04a240e46dabc764bdb96a8ebae60421ffccfe1
Edgar Trujillo <tedgar@google.com>
2026-06-23 20:42:40 +0000

    merge with test

--- backend/main.py
+++ backend/main.py
@@ -57,9 +57,7 @@ from src.workflows_executor.workflows_executor_controller import (
     router as workflows_executor_router,
 )
 from src.workspaces.workspace_controller import router as workspace_router
-from src.system_settings.system_settings_controller import (
-    router as system_settings_router,
-)
+from src.agents.agent_controller import router as agent_router
 
 
 def configure_cors(app):
@@ -184,4 +182,4 @@ app.include_router(brand_guideline_router)
 app.include_router(workflow_router)
 app.include_router(workflows_executor_router)
 app.include_router(workbench_router)
-app.include_router(system_settings_router)
+app.include_router(agent_router)
