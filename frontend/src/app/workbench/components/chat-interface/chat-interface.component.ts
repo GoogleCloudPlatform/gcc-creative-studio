2797ab5fd6d6e9f3020796b09c3daa6dacde0511
Edgar Trujillo <tedgar@google.com>
2026-06-25 20:34:48 +0000

    adjust styles in storyboard

--- frontend/src/app/workbench/components/chat-interface/chat-interface.component.ts
+++ frontend/src/app/workbench/components/chat-interface/chat-interface.component.ts
@@ -225,6 +225,18 @@ export class ChatInterfaceComponent implements OnInit, AfterViewChecked {
       const storyboardId = params['storyboardId'];
       const sessionId = params['sessionId'];
 
+      const isExplicitNewChat =
+        !sessionId &&
+        !storyboardId &&
+        this.lastWorkspaceId === workspaceId;
+
+      if (isExplicitNewChat) {
+        this.isLoadingHistory.set(false);
+        return;
+      }
+
+      this.isLoadingHistory.set(true);
+
       // Always load sessions first to populate the sessions dropdown
       this.agentChatService.getSessions(workspaceId).subscribe({
         next: (
@@ -314,17 +326,6 @@ export class ChatInterfaceComponent implements OnInit, AfterViewChecked {
               this.isLoadingHistory.set(false);
             }
           } else {
-            // Mismatched session/storyboard or no session in URL
-            const isExplicitNewChat =
-              !sessionId &&
-              !storyboardId &&
-              this.lastWorkspaceId === workspaceId;
-
-            if (isExplicitNewChat) {
-              this.isLoadingHistory.set(false);
-              return;
-            }
-
             if ((sessionId || storyboardId) && this.lastWorkspaceId === null) {
               handleErrorSnackbar(
                 this.snackBar,
@@ -416,7 +417,7 @@ export class ChatInterfaceComponent implements OnInit, AfterViewChecked {
           },
         });
     } else {
-      this.isLoadingHistory.set(false);
+      // Do not reset isLoadingHistory to false if workspace is still loading/null
     }
   }
 
