54f6b89f5499477718dd50c850f461e3d8326b7a
Edgar Trujillo <tedgar@google.com>
2026-07-08 20:34:00 +0000

    change api_transport vertex to grpc

--- backend/src/agents/agent_service.py
+++ backend/src/agents/agent_service.py
@@ -44,7 +44,7 @@ logger = logging.getLogger(__name__)
 vertexai.init(
     project=config_service.PROJECT_ID,
     location=config_service.WORKFLOWS_LOCATION,
-    api_transport="rest",  # Options: "grpc" or "rest"
+    api_transport="grpc",  # Options: "grpc" or "rest"
 )
 
 AGENT_REASONING_ENGINES = {
